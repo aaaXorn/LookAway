688e8cea67875efca5ef70be364eb08f31c7e8fe
aaaaaaaaaa <lcrs.rp@gmail.com>
2022-06-21 13:34:04 -0300

    StreamingAssets funfando no build

--- Assets/Scripts/AssetLoading/SA_F.cs
+++ Assets/Scripts/AssetLoading/SA_F.cs
@@ -7,7 +7,12 @@ public class SA_F
 {
 	public static string FileLocation(string relativePath)
 	{
+		#if UNITY_EDITOR
 		return "file://" + Path.Combine(Application.streamingAssetsPath,
 										relativePath);
+		#else
+		return "jar:file://" + Path.Combine(Application.streamingAssetsPath,
+										relativePath);
+		#endif
 	}
 }
