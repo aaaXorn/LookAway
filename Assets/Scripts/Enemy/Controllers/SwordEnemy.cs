4f3f55a08d86a46b183555141d3cee228a09161f
Xorn <lcrs.rp@gmail.com>
2022-06-26 18:51:23 -0300

    AIs inimigo 1 e 2

---
+++ Assets/Scripts/Enemy/Controllers/SwordEnemy.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordEnemy : EnemyControl
+{
+    protected virtual void StateApproach()
+	{
+		Vector3 go_to = PlayerTransf.position - transform.position;
+		//direção
+		Vector3 dir = go_to.normalized;
+		
+		//rotação
+		Quaternion rot = Quaternion.LookRotation(dir, Vector3.up);
+		rot = new Quaternion(transform.rotation.x, rot.y, transform.rotation.z, rot.w);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, rot_spd);
+
+		if(go_to.magnitude > max_approach_range)
+		{
+			//movimento
+			Control.SimpleMove(dir * base_speed);
+		}
+		
+		//continua se movendo
+		if(atk_cd > 0)
+			atk_cd--;
+		//ataca
+		else
+		{
+			//distância entre o inimigo e o player
+			float dist = go_to.magnitude;
+			//melee
+			if(dist <= melee_atk_range)
+			{
+				AnimHit(0);
+				
+				currentState = State.Attack;
+			}
+		}
+	}
+	
+	protected virtual void PostAttackState()
+	{
+		currentState = State.Approach;
+	}
+	
+	public override void Dead()
+	{
+		anim.SetTrigger("Dead");
+			
+		currentState = State.Dead;
+		
+		PlayerEquipment.Instance.SwordXP(1);
+	}
+}
---
+++ Assets/Scripts/Enemy/Health/SwordEnemyHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordEnemyHealth : EnemyHealth
+{
+    private SwordEnemy MookC;
+	
+	protected override void Start()
+	{
+		EnemyC = GetComponent<EnemyControl>();
+		MookC = GetComponent<SwordEnemy>();
+		
+		hp = max_hp;
+	}
+	
+	public override void TakeDamage(int dmg)
+	{
+		//não toma dano se inativo ou resetando
+		if(EnemyC.currentState == EnemyControl.State.Inactive ||
+		   EnemyC.currentState == EnemyControl.State.Reset ||
+		   EnemyC.currentState == EnemyControl.State.Dead)
+			return;
+		
+		hp -= dmg;
+		
+		if(hp > max_hp) hp = max_hp;
+		else if(hp <= 0)
+		{
+			MookC.Dead();
+		}
+	}
+}
