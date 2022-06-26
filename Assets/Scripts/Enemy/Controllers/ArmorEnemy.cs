c429c7b4720dfa5373deb73a3e27ffccfa512a56
Xorn <lcrs.rp@gmail.com>
2022-06-26 19:25:33 -0300

    inimigo 3

--- Assets/Scripts/Enemy/Controllers/ArmorEnemy.cs
+++ Assets/Scripts/Enemy/Controllers/ArmorEnemy.cs
@@ -4,5 +4,44 @@ using UnityEngine;
 
 public class ArmorEnemy : EnemyControl
 {
-    
+    protected virtual void StateApproach()
+	{
+		Vector3 go_to = PlayerTransf.position - transform.position;
+		//direção
+		Vector3 dir = go_to.normalized;
+		
+		//rotação
+		Quaternion rot = Quaternion.LookRotation(dir, Vector3.up);
+		rot = new Quaternion(transform.rotation.x, rot.y, transform.rotation.z, rot.w);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, rot_spd);
+
+		//movimento
+		Control.SimpleMove(-dir * base_speed);
+		
+		//continua se movendo
+		if(atk_cd > 0)
+			atk_cd--;
+		//ataca
+		else
+		{
+			//distância entre o inimigo e o player
+			float dist = go_to.magnitude;
+			//ranged
+			if(dist <= ranged_atk_range)
+			{
+				SpecialHit(0);
+				
+				currentState = State.Special;
+			}
+		}
+	}
+	
+	public override void Dead()
+	{
+		anim.SetTrigger("Dead");
+			
+		currentState = State.Dead;
+		
+		PlayerEquipment.Instance.ArmorXP(1);
+	}
 }
